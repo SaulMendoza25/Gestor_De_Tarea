f080e4e6111a1905ea4170b0fd60c19c3e1b8a24
Saul Samir Mendoza Moreira <saulmendoza6000@gmail.com>
2024-10-16 23:26:54 -0500

    Managment Tasks docomentacion 0.2

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"app"},{"l":"gestion_de_tareas.controller"},{"l":"gestion_de_tareas.model"},{"l":"gestion_de_tareas.view"}];updateSearchResults();
